ab5a5a6e09f7bbb536623a62cad4e7bd1af2ddac
luhaoyu <haoyu3@163.com>
2024-07-08 18:39:13 +0800

    init

--- src-tauri/src/window/setup.rs
+++ src-tauri/src/window/setup.rs
@@ -1,6 +1,19 @@
 use std::error::Error;
-use tauri::App;
+use tauri::{App, Manager};
+use tauri::menu::MenuBuilder;
 
 pub fn setup(app: &mut App) -> Result<(), Box<dyn Error>> {
+    // 这里 `"quit".to_string()` 定义菜单项 ID，第二个参数是菜单项标签。
+    let menu = MenuBuilder::new(app)
+        .copy()
+        .paste()
+        .separator()
+        .undo()
+        .redo()
+        .text("open-url", "Open URL")
+        .check("toggle", "Toggle")
+        .icon("show-app", "Show App", app.default_window_icon().cloned().unwrap())
+        .build()?;
+    app.set_menu(menu)?;
     Ok(())
 }
