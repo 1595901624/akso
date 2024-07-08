88e79e46f8c56b5c9102926f9885be11cb0edacf
luhaoyu <haoyu3@163.com>
2024-07-08 17:06:59 +0800

    init

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import { createApp } from "vue";
-import "./style.css";
-import App from "./App.vue";
-
-import "ant-design-vue/dist/antd.css";
-import Antd from "ant-design-vue";
-
-var app = createApp(App);
-app.use(Antd).mount("#app");
