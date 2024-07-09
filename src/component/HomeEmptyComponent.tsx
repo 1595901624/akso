07bf7d45a13a6d44a554f115676dceb8bac8111e
luhaoyu <haoyu3@163.com>
2024-07-09 13:40:27 +0800

    xx

---
+++ src/component/HomeEmptyComponent.tsx
@@ -0,0 +1,9 @@
+function HomeEmptyComponent() {
+  return (
+    <div className="absolute flex flex-col justify-center items-center h-screen w-screen">
+      <div onClick={() => {}}>选择一个 APK 文件</div>
+    </div>
+  );
+}
+
+export default HomeEmptyComponent;
