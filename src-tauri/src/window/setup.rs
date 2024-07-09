bec151a250a977e93f02e5c88440ee6017de51fa
luhaoyu <haoyu3@163.com>
2024-07-09 09:19:00 +0800

    add test menu

--- src-tauri/src/window/setup.rs
+++ src-tauri/src/window/setup.rs
@@ -1,18 +1,33 @@
 use std::error::Error;
-use tauri::{App, Manager};
-use tauri::menu::MenuBuilder;
+
+use tauri::App;
+use tauri::menu::{MenuBuilder, SubmenuBuilder};
 
 pub fn setup(app: &mut App) -> Result<(), Box<dyn Error>> {
     // 这里 `"quit".to_string()` 定义菜单项 ID，第二个参数是菜单项标签。
-    let menu = MenuBuilder::new(app)
+    // let submenu = MenuBuilder::new(app)
+    //     .copy()
+    //     .paste()
+    //     .separator()
+    //     .undo()
+    //     .redo()
+    //     .text("open-url", "Open URL")
+    //     .check("toggle", "Toggle")
+    //     .icon("show-app", "Show App", app.default_window_icon().cloned().unwrap())
+    //     .build()?;
+    // let menu = MenuBuilder::new(app);
+    // menu.add_item(MenuItem::Submenu("File", submenu));
+    // app.set_menu(menu)?;
+
+    let submenu = SubmenuBuilder::new(app, "File")
         .copy()
         .paste()
         .separator()
         .undo()
         .redo()
-        .text("open-url", "Open URL")
-        .check("toggle", "Toggle")
-        .icon("show-app", "Show App", app.default_window_icon().cloned().unwrap())
+        .build()?;
+    let menu = MenuBuilder::new(app)
+        .item(&submenu)
         .build()?;
     app.set_menu(menu)?;
     Ok(())
