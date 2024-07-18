7f546b4dcd57cd6a1e8821298c66894556189351
luhaoyu <haoyu3@163.com>
2024-07-18 13:58:13 +0800

    add dialog

--- src/component/HomeEmptyComponent.tsx
+++ src/component/HomeEmptyComponent.tsx
@@ -1,9 +1,29 @@
+import {open} from '@tauri-apps/plugin-dialog';
+
+
+type HomeEmptyComponentProps = {
+    onSelectFile: (file: string) => void;
+}
+
 function HomeEmptyComponent() {
-  return (
-    <div className="absolute flex flex-col justify-center items-center h-screen w-screen">
-      <div onClick={() => {}}>选择一个 APK 文件</div>
-    </div>
-  );
+    return (
+        <div className="absolute flex flex-col justify-center items-center h-screen w-screen" onClick={async () => {
+            const file = await open({
+                multiple: false,
+                directory: false,
+                title: '选择一个 APK 文件',
+                filters: [
+                    {name: 'APK', extensions: ['apk']},
+                ],
+            });
+            if (file == null) {
+                return;
+            }
+            console.log(file);
+        }}>
+            <p>选择一个 APK 文件</p>
+        </div>
+    );
 }
 
 export default HomeEmptyComponent;
