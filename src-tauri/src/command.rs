0a2dca5681552740e4d67d19c1f5b3c399c183c3
luhaoyu <haoyu3@163.com>
2024-07-18 17:00:33 +0800

    add bin

---
+++ src-tauri/src/command.rs
@@ -0,0 +1,9 @@
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+#[tauri::command]
+fn get_app_manifest() -> String {
+    AAPT2
+}
