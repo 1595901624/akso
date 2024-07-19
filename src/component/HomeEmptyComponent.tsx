cdaa036d5261513b863ee6f29c8dee863360ca45
luhaoyu <haoyu3@163.com>
2024-07-19 13:56:20 +0800

    add manifest model

--- src/component/HomeEmptyComponent.tsx
+++ src/component/HomeEmptyComponent.tsx
@@ -1,30 +1,30 @@
-import {FileResponse, open} from '@tauri-apps/plugin-dialog';
-
+import { FileResponse, open } from "@tauri-apps/plugin-dialog";
 
 type HomeEmptyComponentProps = {
-    onSelectFile?: (file: FileResponse) => void;
-}
+  onSelectFile?: (file: FileResponse) => void;
+};
 
 function HomeEmptyComponent(props: HomeEmptyComponentProps) {
-    return (
-        <div className="absolute flex flex-col justify-center items-center h-screen w-screen" onClick={async () => {
-            const file: FileResponse | null = await open({
-                multiple: false,
-                directory: false,
-                title: '选择一个 APK 文件',
-                filters: [
-                    {name: 'APK', extensions: ['apk']},
-                ],
-            });
-            if (file == null) {
-                return;
-            }
-            props.onSelectFile?.(file);
-            console.log(file);
-        }}>
-            <p>选择一个 APK 文件</p>
-        </div>
-    );
+  return (
+    <div
+      className="absolute flex flex-col justify-center items-center h-screen w-screen"
+      onClick={async () => {
+        const file: FileResponse | null = await open({
+          multiple: false,
+          directory: false,
+          title: "选择一个 APK 文件",
+          filters: [{ name: "APK", extensions: ["apk", "apk.1"] }],
+        });
+        if (file == null) {
+          return;
+        }
+        props.onSelectFile?.(file);
+        console.log(file);
+      }}
+    >
+      <p>选择一个 APK 文件</p>
+    </div>
+  );
 }
 
 export default HomeEmptyComponent;
