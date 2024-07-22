75a013ee2813224878459cb4dcb559b6bbef65b0
luhaoyu <haoyu3@163.com>
2024-07-22 13:16:07 +0800

    optimize aapt2

--- src-tauri/src/command.rs
+++ src-tauri/src/command.rs
@@ -1,12 +1,13 @@
 use std::fs::File;
 use std::io::Read;
-use aapt2::aapt2::aapt2::AAPT2;
+use std::path::PathBuf;
+
 use aapt2::model::manifest::Manifest;
-use std::path::{Path, PathBuf};
 use sha::{sha1, sha256};
 use sha::utils::{Digest, DigestExt};
-use crate::model::file_info::FileInfo;
 
+use crate::model::file_info::FileInfo;
+use crate::platform;
 #[tauri::command]
 pub fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
@@ -18,16 +19,7 @@ pub fn greet(name: &str) -> String {
 #[tauri::command(rename_all = "snake_case")]
 pub fn get_app_manifest(apk_path: String) -> Manifest {
     println!("apk path: {}", apk_path);
-    let exe_path = std::env::current_exe().unwrap();
-    let exe_dir = exe_path.parent().unwrap();
-    // todo by any platform
-    let aapt2_path = exe_dir.join("aapt2.exe");
-
-    println!("aapt2 path: {}", aapt2_path.display());
-    let aapt2 = AAPT2::from(aapt2_path);
-    // if let Ok(manifest) = aapt2.dump_badging(PathBuf::from(apk_path)) {
-    //     return manifest;
-    // }
+    let aapt2 = platform::create_aapt2();
 
     match aapt2.dump_badging(PathBuf::from(apk_path)) {
         Ok(manifest) => {
--- src-tauri/src/platform/macos.rs
+++ src-tauri/src/platform/macos.rs
@@ -1,8 +1,6 @@
-use std::path::PathBuf;
-
-/// exe 路径
-/// macosx: aapt2
-pub(crate) fn get_exe_path() -> PathBuf {
+/// macos的 exe 路径
+/// macos: aapt2
+pub(crate) fn get_exe_path() -> std::path::PathBuf {
     let exe_path = std::env::current_exe().unwrap();
     let exe_dir = exe_path.parent().unwrap();
     return exe_dir.join("aapt2");
