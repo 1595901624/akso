ded35f1785ff89d4b92ea589573cb6c5de6ad146
luhaoyu <haoyu3@163.com>
2024-07-22 13:02:24 +0800

    add platform crate

---
+++ src-tauri/src/platform/mod.rs
@@ -0,0 +1,34 @@
+use std::path::PathBuf;
+use aapt2::aapt2::aapt2::AAPT2;
+
+#[cfg(target_os = "windows")]
+mod windows;
+#[cfg(target_os = "macos")]
+mod macos;
+
+#[cfg(target_os = "linux")]
+mod linux;
+
+
+#[cfg(target_os = "macos")]
+fn get_aapt2_path() -> PathBuf {
+    return macos::get_exe_path();
+}
+
+#[cfg(target_os = "windows")]
+fn get_aapt2_path() -> PathBuf {
+    return windows::get_exe_path();
+}
+
+#[cfg(target_os = "linux")]
+fn get_aapt2_path() -> PathBuf {
+    return linux::get_exe_path();
+}
+
+fn create_aapt2() -> AAPT2 {
+    AAPT2::from(get_aapt2_path())
+}
+
+fn start_jadx(apk_path: PathBuf) {
+
+}
---
+++ src-tauri/src/platform/windows.rs
@@ -0,0 +1,9 @@
+use std::path::PathBuf;
+
+/// exe 路径
+/// windows: aapt2
+pub(crate) fn get_exe_path() -> PathBuf {
+    let exe_path = std::env::current_exe().unwrap();
+    let exe_dir = exe_path.parent().unwrap();
+    return exe_dir.join("aapt2.exe");
+}
