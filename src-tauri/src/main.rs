624004b8682dfaaffa732e52831657664a3e5962
luhaoyu <haoyu3@163.com>
2024-07-22 13:02:24 +0800

    add platform crate

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -6,6 +6,7 @@ mod window;
 mod model;
 
 mod event;
+mod platform;
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 // #[tauri::command]
 // fn greet(name: &str) -> String {
